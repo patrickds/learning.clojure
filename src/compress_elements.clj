a7d555b513629f2e3e2d84a1168eaa9fa9da3986
Patrick dos Santos <patrickds@outlook.com>
2017-12-27 22:08:10 -0200

    Refactor compress

--- src/compress_elements.clj
+++ src/compress_elements.clj
@@ -1,11 +1,12 @@
 (ns compress-elements)
 
+(defn- drop-if-equal-previous
+  [acc current]
+  (let [previous (last acc)]
+    (cond 
+      (= previous current) acc
+      :else (concat acc [current]))))
+
 (defn compress
   [coll]
-  (reduce (fn
-    [acc current]
-    (let [previous (last acc)]
-      (cond 
-        (= previous current) acc
-        :else (concat acc [current]))))
-      '() coll))
+  (reduce drop-if-equal-previous '() coll))
